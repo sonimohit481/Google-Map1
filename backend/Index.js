d475eb13dac95aa61766c8893448b03c262f2507
sonimohit481 <sonimohit481@gmail.com>
2022-12-29 16:15:41 +0530

    yes

---
+++ backend/Index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+app.use(cors({ origin: "*" }));
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// ---------
+
+app.get("/", (req, res) => {
+    let arr = []
+    arr[0] = {
+        name: "home",
+        lat: Math.random() * (24 - 21) + 21,
+        lng: Math.random() * (84 - 81) + 81,
+    }
+    for (let i = 0; i < 2; i++) {
+        arr[i + 1] = {
+            name: i + 1,
+            lat: Math.random() * (24 - 21) + 21,
+            lng: Math.random() * (84 - 81) + 81,
+        }
+    }
+
+    arr.push(
+        arr[0]
+    )
+    res.send(JSON.stringify(
+        arr
+    ))
+})
+
+app.listen(8080, () => {
+    console.log("Listening on post 8080")
+});
